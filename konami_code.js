d5ae83d3ce587b8a99c44cb054593ad02a354787
Mike Pokalsky <mpoksky1989@gmail.com>
2020-08-31 18:59:51 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,16 +1,30 @@
 const codes = [
-  "ArrowUp",
-  "ArrowUp",
-  "ArrowDown",
-  "ArrowDown",
-  "ArrowLeft",
-  "ArrowRight",
-  "ArrowLeft",
-  "ArrowRight",
-  "b",
-  "a"
+ "ArrowUp",
+ "ArrowUp",
+ "ArrowDown",
+ "ArrowDown",
+ "ArrowLeft",
+ "ArrowRight",
+ "ArrowLeft",
+ "ArrowRight",
+ "b",
+ "a"
 ];
 
+
 function init() {
-  // your code here
+var index = 0;
+
+document.body.addEventListener('keydown', function(e) {
+   if (codes[index] === e.key) {
+       index++;
+
+       if (index === codes.length) {
+           alert("Hurray");
+           index = 0;
+       }
+   } else {
+       index = 0;
+   }
+})
 }
