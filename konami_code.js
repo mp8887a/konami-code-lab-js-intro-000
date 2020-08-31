af98eb875630e77a360c9eec862dbf9ff3e9b485
Mike Pokalsky <mpoksky1989@gmail.com>
2020-08-31 17:10:41 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,22 @@ const codes = [
   "a"
 ];
 
+
 function init() {
-  // your code here
+// Write your JavaScript code inside the init() function
+var index = 0;
+const getElement = document.querySelector('body');
+
+getElement.addEventListener('keydown', function(e) {
+    if (code[index] === e.which || code[index] === e.detail) {
+        index++;
+
+        if (index === 10) {
+            alert("CONGRATS!");
+            index = 0;
+        }
+    } else {
+        index = 0;
+    }
+})
 }
